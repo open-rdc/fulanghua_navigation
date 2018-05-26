270cb76515f044c6cd0366ef42c9463d000385a2
Norihiro KONDO <38202582+Norihiro02@users.noreply.github.com>
2018-05-26 23:42:42 +0900

    Update waypoints_nav.cpp
    再度目的地(waypoint)へのglobalpathを生成する条件を変更
    旧：10秒で目的地に着かない&&10秒間ロボットが動かない。
    新：10秒間ロボットが動かない。のみ

--- fulanghua_waypoints_nav/src/waypoints_nav.cpp
+++ fulanghua_waypoints_nav/src/waypoints_nav.cpp
@@ -370,7 +370,8 @@ public:
                             throw SwitchRunningStatus();
                         
                         double time = ros::Time::now().toSec();
-                        if(time - start_nav_time > 10.0 && time - last_moved_time_ > 10.0) {
+                        //if(time - start_nav_time > 10.0 && time - last_moved_time_ > 10.0) {
+                        if(time - last_moved_time_ > 10.0) {//10秒間ロボットが動かなかった時の対処
                             ROS_WARN("Resend the navigation goal.");
                             std_srvs::Empty empty;
                             clear_costmaps_srv_.call(empty);
